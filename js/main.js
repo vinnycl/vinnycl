29fcdd6d494bbd5bb37caaeead46c75d20c77416
unknown <vinicius.loureiro@EW7OL1771.arca.corp.br>
2015-05-15 18:41:58 -0300

    site

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE'],
-		anchors: ['firstPage', 'secondPage', 'thirdPage'],
+		anchors: ['Info', 'Education', 'Skills', 'Works', 'Contact'],
 		navigation: true,
+		menu: '#menu',
 		navigationPosition: 'right',
 		slidesNavigation: true,
 		controlArrows: false,
